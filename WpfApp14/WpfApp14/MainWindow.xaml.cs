4b6a56f700a05cc028f9220b9202bde853622bee
pantinaEA <61845605+pantinaEA@users.noreply.github.com>
2020-03-13 14:17:28 +0300

    Добавлены картинки крестика и нолика
    Еще добавлен метод кнопки с чередованием крестика и нолика

--- WpfApp14/WpfApp14/MainWindow.xaml.cs
+++ WpfApp14/WpfApp14/MainWindow.xaml.cs
@@ -20,7 +20,9 @@ namespace WpfApp14
     /// </summary>
     public partial class MainWindow : Window
     {
-        Boolean XMovement = false; // для чередования хода
+        Boolean X = false; // для чередования хода
+        BitmapImage bmpImgX;
+        BitmapImage bmpImg0;
 
         CurrentCell[] cellStates = new CurrentCell[9]
             {
@@ -32,6 +34,10 @@ namespace WpfApp14
         public MainWindow()
         {
             InitializeComponent();
+            Uri uriImg0 = new Uri(@"pack://application:,,,/Resources/o.png", UriKind.Absolute);
+            Uri uriImgX = new Uri(@"pack://application:,,,/Resources/x.png", UriKind.Absolute);
+            bmpImg0 = GetBitmapImage(uriImg0);
+            bmpImgX = GetBitmapImage(uriImgX);
             for (int i = 0; i < LayoutRoot.Children.Count; i++)
             {
                 if (LayoutRoot.Children[i] is Button)
@@ -41,8 +47,37 @@ namespace WpfApp14
                 }
             }
         }
+        BitmapImage GetBitmapImage(Uri uri)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = uri;
+            bitmapImage.EndInit();
+            return bitmapImage;
+        }
         private void Button_Click(object sender, EventArgs e)
-        { }
+        {
+            Button btn = (Button)sender;
+            Image img = (Image)btn.Content;
+            int btnNumber = Convert.ToInt32((string)btn.Tag);
+
+            if (X == true && cellStates[btnNumber] == CurrentCell.NotSelected)
+            {
+                img.Source = bmpImgX;
+                cellStates[btnNumber] = CurrentCell.X;
+                X = false;
+
+            }
+            else if (cellStates[btnNumber] == CurrentCell.NotSelected)
+            {
+                img.Source = bmpImg0;
+                cellStates[btnNumber] = CurrentCell.O;
+                X = true;
+            }
+
 
         }
+    }
+
 }
+
